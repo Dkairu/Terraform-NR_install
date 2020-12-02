20ef4a69a86a9ce446890268623298d9b95cab75
dkairu <dontke1@gmail.com>
2020-12-02 17:25:21 -0500

    update terraform script

--- nr-infra_install-example.tf
+++ nr-infra_install-example.tf
@@ -2,21 +2,18 @@ provider "aws" {
       region = "us-east-1"
 }
 
-variable "count" {
-  	default = 3
-}
 variable "azs" {
-  type = "list"
+  type = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 resource "aws_instance" "dan_terraform_example" {
-        ami = "ami-5c66ea23"
+        ami = "ami-0817d428a6fb68645"
         instance_type = "t2.micro"
-        availability_zone = "${element(var.azs, count.index)}"
-        key_name = "test-key"
-        security_groups= ["EC2 Security"]
-        count = "${var.count}"
-	tags {
+        availability_zone = element(var.azs, count.index)
+        key_name = "ec2"
+        security_groups= ["yooo-8968"]
+        count = 3
+	tags ={
         Name = "NewRelic Web server ${count.index+1}"
 	 }
 
@@ -24,15 +21,15 @@ resource "aws_instance" "dan_terraform_example" {
 		#!/bin/bash
 		sudo apt-get install apache2 -y
 		sudo sed -i 's/It\ works!/Server\ ${count.index+1} works!/g' /var/www/html/index.html
-		echo "license_key: <ENTER LICENSE KEY HERE>" | sudo tee -a /etc/newrelic-infra.yml
+		echo "license_key: <ENTER LICENSE HERE>" | sudo tee -a /etc/newrelic-infra.yml
 		curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
-			printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
+			printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt bionic main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
 		sudo apt-get update
 		sudo apt-get install newrelic-infra -y
 		EOF
 }
 resource "aws_elb" "dan_elb" {
-	instances = ["${aws_instance.dan_terraform_example.*.id}"]
+	instances = aws_instance.dan_terraform_example.*.id
 	cross_zone_load_balancing = true
 	availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 	name = "ELB-TF"
@@ -44,6 +41,9 @@ resource "aws_elb" "dan_elb" {
  	}
 }
 output "addresses" {
-	value = ["${aws_instance.dan_terraform_example.*.public_ip}"]
-	value = ["${aws_elb.dan_elb.dns_name}"]
+	value = aws_instance.dan_terraform_example.*.public_ip
+	//value = ["${aws_elb.dan_elb.dns_name}"]
+}
+output "elb" {
+	value = aws_elb.dan_elb.dns_name
 }
